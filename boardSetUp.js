aaa1f429d4cacacc0869b24089f7e8a035e5a5c4
Yashdeep Singh Fauzdar <yashdeep.fauzdar@gmail.com>
2025-02-11 15:14:11 +0530

    Small optimisations & getting ready for future work

--- boardSetUp.js
+++ boardSetUp.js
@@ -1,4 +1,4 @@
-const chessBoard = document.getElementById("chessBoard");
+const chessBoard = document.getElementById("chess-board");
 let isBoardFlipped = false;
 let boardPieces, activeColour, castlingRights, enPassantSquare, halfmoveClock, fullmoveNumber;
 
@@ -82,7 +82,10 @@ function parseFen(fen) {
         }
         if (!(boardPieces.includes("K") && boardPieces.includes("k"))) {
             isFenValid = false;
-            reasonsForInvalidFen.push("The piece placement described by the provided FEN doesn’t include the position(s) of one or both king(s).");
+            reasonsForInvalidFen.push("The provided FEN doesn’t include the position(s) of one or both king(s).");
+        } else if(boardPieces.filter(x => x === "K").length > 1 || boardPieces.filter(x => x === "k").length > 1) {
+            isFenValid = false;
+            reasonsForInvalidFen.push("The provided FEN specifies the position of one or both king(s) more than once.");
         }
     }
     if (isFenValid == false) {
@@ -93,7 +96,8 @@ function parseFen(fen) {
         return false;
     }
 }
-function updateChessBoard(isFlipped, fen) {
+function updateChessBoard(isFlipped) {
+    const fen = document.getElementById("fen-input").value.trim();
     const fenParseResult = parseFen(fen);
     if (fenParseResult) {
         let errors = "";
@@ -102,7 +106,7 @@ function updateChessBoard(isFlipped, fen) {
             errors += i + ". " + fenParseResult[i - 1] + "<br />";
             i--;
         }
-        document.getElementById("fenVerifier").innerHTML = `<svg height = "24px" viewBox = "0 -960 960 960" width = "24px" fill = "red">
+        document.getElementById("fen-validity-indicator").innerHTML = `<svg height = "24px" viewBox = "0 -960 960 960" width = "24px" fill = "red">
                 <path d = "m336-280 144-144 144 144 56-56-144-144 144-144-56-56-144 144-144-144-56 56 144 144-144 144 56 56ZM480-80q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
             </svg>
             <div>
@@ -111,10 +115,10 @@ function updateChessBoard(isFlipped, fen) {
             </div>`;
         return;
     }
-    document.getElementById("fenVerifier").innerHTML = `<svg height = "24px" viewBox = "0 -960 960 960" width = "24px" fill = "#5f6368">
+    document.getElementById("fen-validity-indicator").innerHTML = `<svg height = "24px" viewBox = "0 -960 960 960" width = "24px" fill = "var(--colour)" opacity = "0.6">
             <path d = "m424-296 282-282-56-56-226 226-114-114-56 56 170 170Zm56 216q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Z" />
         </svg>`;
-    const prevBoardRanks = document.querySelectorAll(".boardRank");
+    const prevBoardRanks = document.querySelectorAll(".board-rank");
     for (let i = 0; i < prevBoardRanks.length; i++) {
         prevBoardRanks[i].remove();
     }
@@ -126,7 +130,7 @@ function updateChessBoard(isFlipped, fen) {
     }
     for (let rank = isFlipped ? 1 : 8; [1, 2, 3, 4, 5, 6, 7, 8].includes(rank); rank = isFlipped ? rank+1 : rank-1) {
         const rankElement = document.createElement("div");
-        rankElement.className = "boardRank";
+        rankElement.className = "board-rank";
         let isLightSquare = rank%2 > 0 ? true : false;
         if (isFlipped) {
             isLightSquare = !isLightSquare;
@@ -134,14 +138,14 @@ function updateChessBoard(isFlipped, fen) {
         for (i in boardFiles) {
             isLightSquare = !isLightSquare;
             const square = document.createElement("div");
-            square.className = "boardSquare";
+            square.className = "board-square";
             if (isLightSquare) {
-                square.className += " lightBoardSquare";
+                square.className += " light-board-square";
             }
             square.id = `${boardFiles[i]}${rank}`;
             pieceOnSquare = boardPieces[squareNumber];
             if (pieceOnSquare != "") {
-                square.className += " containsPiece";
+                square.className += " contains-piece";
                 square.style.backgroundImage = `var(--${pieceOnSquare})`;
             }
             rankElement.appendChild(square);
@@ -151,5 +155,24 @@ function updateChessBoard(isFlipped, fen) {
     }
 }
 
-document.getElementById("fenInput").value = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
-updateChessBoard(false, document.getElementById("fenInput").value);
+const parameters = location.search.replace(/%20/g, " ").split("?");
+if (parameters != "") {
+    const firstParameter = parameters[1];
+    switch (firstParameter.slice(0, 4)) {
+        case "fen=":
+            document.getElementById("fen-input").value = firstParameter.slice(4, firstParameter.length);
+            if (parameters[2] && parameters[2].includes("flip")) {
+                isBoardFlipped = true;
+            }
+            break;
+        case "pgn=":
+            document.getElementById("pgn-input").value = firstParameter.slice(4, firstParameter.length);
+            if (parameters[2] && parameters[2] == "flip") {
+                isBoardFlipped = true;
+            }
+            break;
+        case "flip":
+            isBoardFlipped = true;
+    }
+}
+updateChessBoard(isBoardFlipped);
