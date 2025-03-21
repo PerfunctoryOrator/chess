c68cd249d420c1e379f070860b4b51977d288319
Yashdeep Singh Fauzdar <yashdeep.fauzdar@gmail.com>
2025-03-21 15:11:44 +0530

    Update .gitignore

---
+++ config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCuoOWv2X80AsTTCtj-yg1ItjZJQNDA8ag",
+    authDomain: "perforator-chess.firebaseapp.com",
+    databaseURL: "https://perforator-chess-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "perforator-chess",
+    storageBucket: "perforator-chess.firebasestorage.app",
+    messagingSenderId: "1032974911551",
+    appId: "1:1032974911551:web:8539eccd9043a11b2e108a"
+};
